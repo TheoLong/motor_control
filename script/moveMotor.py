b8e73d26b7be80580da3983eec9bef20cb9f4b73
TheoLong <TheoLoong@gmail.com>
2015-11-12 13:02:33 -0500

    Update moveMotor.py

--- script/moveMotor.py
+++ script/moveMotor.py
@@ -16,18 +16,18 @@ def moveLeft(velocity):
 	if power>0:
                 GPIO.output("P9_11", GPIO.HIGH);
                 GPIO.output("P9_12", GPIO.LOW);
-                PWM.set_duty_cycle("P9_16",power)
+                PWM.set_duty_cycle("P9_14",power)
 	#if power < 0, move backward
 	if power<0:
 		power=abs(power)
 		GPIO.output("P9_11", GPIO.LOW);
                 GPIO.output("P9_12", GPIO.HIGH);
-                PWM.set_duty_cycle("P9_16",power)
+                PWM.set_duty_cycle("P9_14",power)
 	#if power = 0, stop
 	if power == 0:
 		GPIO.output("P9_11", GPIO.LOW);
                 GPIO.output("P9_12", GPIO.LOW);
-                PWM.set_duty_cycle("P9_16",0)
+                PWM.set_duty_cycle("P9_14",0)
 def moveRight(velocity):
 	print("move left")
 	power=0.0
@@ -40,18 +40,18 @@ def moveRight(velocity):
 	if power>0:
                 GPIO.output("P9_13", GPIO.HIGH);
                 GPIO.output("P9_15", GPIO.LOW);
-                PWM.set_duty_cycle("P9_14",power)
+                PWM.set_duty_cycle("P9_16",power)
 	#if power < 0, move backward
 	if power<0:
 		power=abs(power)
 		GPIO.output("P9_15", GPIO.LOW);
                 GPIO.output("P9_15", GPIO.HIGH);
-                PWM.set_duty_cycle("P9_14",power)
+                PWM.set_duty_cycle("P9_16",power)
 	#if power = 0, stop
 	if power == 0:
 		power=round(power)
 		GPIO.output("P9_13", GPIO.LOW);
                 GPIO.output("P9_15", GPIO.LOW);
-                PWM.set_duty_cycle("P9_14",0)
+                PWM.set_duty_cycle("P9_16",0)
 		
 
